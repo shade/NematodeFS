b4e4d07247400347a4321e4a61628794de73643b
joe <josepht404@gmail.com>
2019-05-04 09:29:05 -0700

    Finish block publish method, so much work to go

--- fs/block.js
+++ fs/block.js
@@ -1,3 +1,4 @@
+let bsv = require('bsv')
 
 
 class Block {
@@ -5,10 +6,22 @@ class Block {
         this.keys = keys
         this.data = data
     }
-    
-    publish () {
+
+    /**
+     * Publishes the entire block to the bitcoin network
+     * 
+     * @param {String} prevTxHash The hash of the transaction we're spending
+     * @param {Number} prevTxIndex The index of the transaction we're spending
+     */
+    publish (prevTxHash, prevTxIndex) {      
         // Create transaction
+        const tx = new bsv.Transaction()
+        tx.setVersion(1)
+        tx.addInput(prevTxHash, prevTxIndex)
+        tx.addData(this.data)
+
         // Push transaction to the network
+        networkPush(tx.serialize())
     }
 }
 
