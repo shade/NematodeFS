fadec5fb178fe5fdd02ddb5f99bf479be9a9ea25
joe <josepht404@gmail.com>
2019-05-04 07:51:27 -0700

    Add in fs header

--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -1,6 +1,10 @@
 <template lang="jade">
   div
-    h1.header Pay4Data
+    div.header
+      .logo Infinite File System
+      .upload__btn 
+        span(style="font-size: 15px; float: left") &#x21EA;&nbsp;
+        | Upload
 </template>
 
 <script>
@@ -19,9 +23,27 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="sass">
-  @import url('https://fonts.googleapis.com/css?family=Source+Code+Pro:300');
+  @import url('https://fonts.googleapis.com/css?family=Vesper+Libre:400|Inconsolata')
 
-  h1.header
-    font-family: Source Code Pro
-    font-weight: 300
+  .header
+    font-family: Vesper Libre
+    max-width: 800px
+    width: 80%
+    margin: auto
+    .logo
+      float: left
+      font-size: 20px
+    .upload__btn
+      float: right
+      font-family: Inconsolata
+      text-transform: uppercase
+      letter-spacing: 1px
+      font-size: 12px
+      cursor: pointer
+      border: 1px solid rgba(140, 20, 252, 1)
+      border-radius: 5px
+      padding: 10px
+      color: rgba(140, 20, 252, 1)
+      background-color: rgba(140, 20, 252, 0.1)
+      line-height: 20px
 </style>
