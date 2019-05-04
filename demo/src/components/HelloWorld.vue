4c5a1ff5efa336671c1eeea9f441aa10ecfbf7c3
joe <josepht404@gmail.com>
2019-05-04 06:07:31 -0700

    Add vue project template

---
+++ demo/src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template lang="jade">
+  div
+    h1.header Pay4Data
+</template>
+
+<script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="sass">
+  @import url('https://fonts.googleapis.com/css?family=Source+Code+Pro:300');
+
+  h1.header
+    font-family: Source Code Pro
+    font-weight: 300
+</style>
