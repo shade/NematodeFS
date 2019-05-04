376d3d4ddd160c170d9528b5361ac2461d18cb0b
joe <josepht404@gmail.com>
2019-05-04 15:24:23 -0700

    Update the deserialization function for folders

--- fs/dir.js
+++ fs/dir.js
@@ -17,4 +17,35 @@ class Folder {
             folderSize += 32
         }
     }
+
+    static validateName() {
+        if (this.name.indexOf('/') !== -1) {
+            throw new Error("Serialization did not work, bad name!")
+        }
+    }
+
+    static deserialize (buffer) {
+        // Add in size
+        this.size = buffer[0] + (buffer[1] << 8)
+        this.mode = buffer[2]
+        
+        // Check if it's dynamic or static 
+        let cursor = 3
+        if (mode & DYNAMIC_FOLDER_BIT) {
+            cursor += 20
+            this.ptr = buffer.slice(3, 3 + 20)
+        } else {
+            cursor += 32
+            this.ptr = buffer.slice(3, 3 + 32)
+        }
+
+        // Get the name length
+        let namelen = buffer[cursor]
+        namelen += buffer[cursor + 1]
+
+        cursor += 2
+
+        this.name = buffer.slice(cursor, cursor + namelen).toString()
+        this.validateName()
+    }
 }
