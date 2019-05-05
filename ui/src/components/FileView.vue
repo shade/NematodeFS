565af8133658427f75eba10b30b246ef0b1f18e2
joe <josepht404@gmail.com>
2019-05-04 22:05:20 -0700

    Abstract out navbar

--- ui/src/components/FileView.vue
+++ ui/src/components/FileView.vue
@@ -1,9 +1,6 @@
 <template lang="jade">
   div
-    div.header
-      .logo
-        img(src="../assets/infinity.png" height="30px" style="float:left;margin-top: -5px;margin-right: 10px;")
-        |Infinite File System
+    Navbar
     br
     div.main__container
       div.entry__container
@@ -38,11 +35,13 @@
 
 <script>
   import FileEntry from './FileEntry.vue'
-  
+  import Navbar from './Navbar.vue'
+
   export default {
     name: 'Home',
     components: {
-      FileEntry
+      FileEntry,
+      Navbar
     },
     mounted () {
       if (!localStorage.getItem('private')) {
@@ -65,29 +64,6 @@
   @import url('https://fonts.googleapis.com/css?family=Inconsolata')
   body
     background-color: #FFF
-
-  .header
-    font-family: Inconsolata
-    max-width: 800px
-    width: 80%
-    margin: auto
-    margin-bottom: 10px
-    .logo
-      float: left
-      font-size: 20px
-    .upload__btn
-      float: left
-      font-family: Inconsolata
-      text-transform: uppercase
-      letter-spacing: 1px
-      font-size: 12px
-      cursor: pointer
-      border: 1px solid rgba(160, 20, 252, 1)
-      border-radius: 5px
-      padding: 10px
-      color: rgba(140, 20, 252, 1)
-      background-color: rgba(140, 20, 252, 0.1)
-      line-height: 20px
 
   .main__container
     width: 80%
---
+++ ui/src/components/Navbar.vue
@@ -0,0 +1,47 @@
+<template lang="jade">
+    div.header
+        .logo
+            img(src="../assets/infinity.png" height="30px" style="float:left;margin-top: -5px;margin-right: 10px;")
+            | Infinite File System
+        .amount ~45.4 actions left
+
+
+</template>
+<script>
+    export default {
+        name: 'Navbar'
+    }
+</script>
+
+<style lang="sass">
+
+    .header
+        font-family: Inconsolata
+        max-width: 800px
+        width: 80%
+        margin: 20px auto
+
+        .logo
+            float: left
+            font-size: 18px
+            color: #555
+        .upload__btn
+            float: left
+            font-family: Inconsolata
+            text-transform: uppercase
+            letter-spacing: 1px
+            font-size: 12px
+            cursor: pointer
+            border: 1px solid rgba(160, 20, 252, 1)
+            border-radius: 5px
+            padding: 10px
+            color: rgba(140, 20, 252, 1)
+            background-color: rgba(140, 20, 252, 0.1)
+            line-height: 20px
+        .amount
+            float: right
+            font-size: 12px
+            cursor: pointer
+            margin-right: 20px
+
+</style>
