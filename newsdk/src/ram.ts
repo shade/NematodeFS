cc01faf56ef5b83affc326ce4032ba03f65f7e56
joe <josepht404@gmail.com>
2019-05-05 15:15:13 -0700

    Update ram, need to add API key

--- newsdk/src/ram.ts
+++ newsdk/src/ram.ts
@@ -1,14 +1,28 @@
-
+import request from 'request-promise'
 import BitIndexSDK from "bitindex-sdk";
 
 let bitindex = new BitIndexSDK()
 
 export default class RAM {
-    static getTx(hash: string): Uint8Array {
-    
+    static getTx(hash: string): JSON {
+        let url = [
+            BITDB_URL,
+            BITDB_API_KEY,
+            query
+        ].join('/')
+
+        return request.get(url)
     }
 
-    static getLastTxData(address: string): Uint8Array {}
+    static getLastTxData(address: string): Uint8Array {
+        let url = [
+            BITDB_URL,
+            BITDB_API_KEY,
+            query
+        ].join('/')
+
+        return request.get(url)
+    }
     
 
     static getUTXOs(pubkey: string): Promise<any> {
