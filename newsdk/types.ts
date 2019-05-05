72577fc0fd3743fcc334edd9f7b47b42be7dd877
joe <josepht404@gmail.com>
2019-05-04 21:07:01 -0700

    Create new types, debating the folder structure still

---
+++ newsdk/types.ts
@@ -0,0 +1,38 @@
+
+interface Dir {
+    // 2 byte number, the size of this entire directory entry
+    record_size: number,
+
+    // 1 byte number: the type of record, folder, file, dynamic, static
+    record_type: number,
+
+    // Pointer to either a static resource, or another pubkey inode, or your own child
+    static_pointer: Uint8Array[32],
+    dynamic_pointer: Uint8Array[20],
+    child_pointer: number
+
+    // 1 byte length for name
+    name_len: number
+
+    // 256 character name, max
+    name: Uint8Array[256]
+}
+
+interface INode {
+    // Tells us if this is a directory, a normal file
+    mode: number,
+    // A 20 byte pubkeyhash related to the type 
+    bitcom_id: Uint8Array[20]
+    // A 8 byte number telling us how large this file is
+    size: number,
+    // A 4 byte number telling us how many children inodes have ever been created
+    // Set to 0 for files
+    children_count: number,
+    // A 4 byte number telling us how many records this has, (only for directories)
+    // Set to 0 for files
+    record_count: number,
+}
+
+interface DirINode implements INode {
+    dirs: Dir[]
+}
